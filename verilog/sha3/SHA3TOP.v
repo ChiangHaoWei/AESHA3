2a267f4eba23d782f08df6cc969a1546b8a1985e
Davis Chou <lxdfs58@gmail.com>
2022-04-28 22:45:08 +0800

    Add SHA3 Top

---
+++ verilog/sha3/SHA3TOP.v
@@ -0,0 +1,23 @@
+module SHA3(
+    in, more, input_valid, out, next, ready
+);
+input [1087:0] in;
+input more, input_valid,;
+output [255:0] out;
+output next, ready;
+
+Ffunction keccak_f(
+
+);
+
+//comb
+always @() begin
+    
+end
+
+//seq
+always @(posedge clk) begin
+    
+end
+
+endmodule
